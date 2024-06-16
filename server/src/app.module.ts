00b4eacd462155c301bed2ac2815597fab353e07
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-06-16 17:19:52 +0200

    feat: Setting up environment variables. Creating User model

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { User } from './users/users.model';
 
 @Module({
   // чтобы контроллеры, провайдеры и др заработали, их нужно зарегестрировать в модуле
@@ -7,16 +10,20 @@ import { SequelizeModule } from '@nestjs/sequelize';
   providers: [],
   // когда нужно установить другие модули (у меня SequelizeModule)
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5252,
-      username: 'postgres',
-      password: '030815',
-      database: 'meet-event',
-      models: [],
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User],
       autoLoadModels: true,
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
