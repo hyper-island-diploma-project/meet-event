b6136c680d6e091566e1553222a24bae44a28533
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-06-17 16:23:49 +0200

    feat: create swagger docs for getAllUsers and createUser

--- server/src/users/users.model.ts
+++ server/src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 // поля которые будут нужны для создания объекта
@@ -8,6 +9,7 @@ interface UserCreationAttribute {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttribute> {
+  @ApiProperty({ example: '1', description: 'Unique identifier' }) //swagger
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -16,6 +18,7 @@ export class User extends Model<User, UserCreationAttribute> {
   })
   id: number;
 
+  @ApiProperty({ example: 'user@email.com', description: 'Email' })
   @Column({
     type: DataType.STRING,
     unique: true,
@@ -23,54 +26,72 @@ export class User extends Model<User, UserCreationAttribute> {
   })
   email: string;
 
+  @ApiProperty({ example: '12345678', description: 'Password' })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   password: string;
 
+  @ApiProperty({ example: 'User', description: 'User name' })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   first_name: string;
 
+  @ApiProperty({ example: 'User', description: 'User last name' })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   last_name: string;
 
+  @ApiProperty({ example: 'Job', description: 'Job title' })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   job_title: string;
 
+  @ApiProperty({ example: 'Company name', description: 'Workplace' })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   workplace: string;
 
+  @ApiProperty({ example: '1 year', description: 'Experience' })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   experience: string;
 
+  @ApiProperty({
+    example: 'https://example.com/images/sample.jpg',
+    description: 'URL of the image',
+  })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   image: string;
 
+  @ApiProperty({
+    example: true,
+    description: 'Indicates whether the user is banned',
+  })
   @Column({
     type: DataType.BOOLEAN,
     defaultValue: false,
   })
   banned: boolean;
 
+  @ApiProperty({
+    example: 'Violation of terms of service',
+    description: 'Reason for banning the user, if applicable',
+  })
   @Column({
     type: DataType.STRING,
     allowNull: true,
