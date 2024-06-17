1605ab968533e2bfd9e06313482b5d44b80db847
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-06-17 15:37:59 +0200

    feat: create get all users and user registration on the server

---
+++ server/src/users/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+export class CreateUserDto {
+  readonly email: string;
+  readonly password: string;
+  readonly first_name: string;
+  readonly last_name: string;
+  readonly job_title: string;
+  readonly workplace: string;
+  readonly experience: string;
+  readonly image: string;
+}
